88562701ef9d920c1e3f6ec4b9273d0288f575ee
wkstevens1993 <wkstevens1993@gmail.com>
2025-09-12 14:28:27 -0400

    update

---
+++ Assets/Scripts/Weapons and Powers/weaponStats.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+[CreateAssetMenu]
+
+public class weaponStats : ScriptableObject
+{
+    public GameObject gunModel;
+    public int shootDamage;
+    public int shootDist;
+    public float shootRate;
+    public int ammoCur;
+    public int ammoMax;
+
+    public bool lazer;
+    public bool spread;
+    public bool headshots;
+
+    public ParticleSystem shootEffect;
+    public AudioClip shootSound;
+    public float shootVol;
+}
