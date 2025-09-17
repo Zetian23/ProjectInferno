fd989cd4c5536908ad35b69c4c307edd875f2611
Zetian23 <alexnewland12@gmail.com>
2025-09-16 20:35:22 -0400

    Forgot something

--- Assets/Scripts/ForcedTeleport.cs
+++ Assets/Scripts/ForcedTeleport.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using UnityEngine;
 
-public class Teleport : MonoBehaviour
+public class ForcedTeleport : MonoBehaviour
 {
     [SerializeField] Transform teleportTarget;
 
