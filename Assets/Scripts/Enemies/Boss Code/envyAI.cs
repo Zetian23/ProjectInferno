12b2d510d3e22bccf6f00193ae327436e02c9796
KingNathanielIsAwesome <nateking32@gmail.com>
2025-09-20 21:57:33 -0400

    Envy boss

--- Assets/Scripts/Enemies/Boss Code/envyAI.cs
+++ Assets/Scripts/Enemies/Boss Code/envyAI.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
@@ -14,16 +15,23 @@ using UnityEngine;
 public class envyAI : sinEnemy
 {
     [SerializeField] GameObject phase3Laser;
+    [SerializeField] GameObject phase2Laser;
     [SerializeField] float downTurretsTime;
+    [SerializeField] float spinCooldownTime;
+    [SerializeField] float spinTime;
 
     List<turretEnemy> turretList;
     float downTurretsTimer;
+    float spinCooldownTimer;
+    float spinTimer;
+    float currYRot;
 
     void Start()
     {
         InitVar(); // This calls the method in sinEnemy that initializes all fields in that script needed for this.
 
         isAttacking = false;            // Initializing that an attack is not happening.
+        isSpinning = false;
 
         IEnumerable<turretEnemy> list = FindObjectsByType<MonoBehaviour>(FindObjectsSortMode.None).OfType<turretEnemy>();
         turretList = new List<turretEnemy>(list);
@@ -37,17 +45,30 @@ public class envyAI : sinEnemy
     void Update()
     {
         attackTimer += Time.deltaTime;  // Ticks the attackTimer up so it can know when to attack based off the attackRate.
+        spinCooldownTimer += Time.deltaTime;  // Ticks the attackTimer up so it can know when to attack based off the attackRate.
 
         checkHealth(500, 250);   // Checks the phases between the health periods.
+        if (!isSpinning)
+        {
+            if (playerInTrigger && canSeePlayer()) { }  // Checks if player is in the trigger and uses the canSeePlayer() method in the Enemy script.
+        }
 
-        if (playerInTrigger && canSeePlayer()) { }  // Checks if player is in the trigger and uses the canSeePlayer() method in the Enemy script.
+        if(gamemanager.instance.GetPhase() >= 2 && !isSpinning && spinCooldownTimer >= spinCooldownTime)
+        {
+            currYRot = transform.rotation.y;
+            isSpinning = true;
+        }
+        else if(gamemanager.instance.GetPhase() >= 2 && isSpinning && spinCooldownTimer >= spinCooldownTime)
+        {
+            StartCoroutine(spinAttack());
+        }
+
+        if (gamemanager.instance.GetPhase() == 3) projectile = phase3Laser;
 
         if (isAttacking)
         {
             rangedAttack();
         }
-
-        if (gamemanager.instance.GetPhase() == 3) projectile = phase3Laser;
 
         CheckTurrets();
     }
@@ -67,5 +88,25 @@ public class envyAI : sinEnemy
             }
             downTurretsTimer = 0;
         }
+    }
+
+    IEnumerator spinAttack()
+    {
+        spinTimer += Time.deltaTime;
+        transform.rotation = Quaternion.Euler(transform.rotation.x, currYRot, transform.rotation.z);
+        if (currYRot > 360) currYRot = 1;
+        else currYRot += 3;
+        if(spinTimer >= .25f && phase2Laser.activeInHierarchy == false)
+        {
+            phase2Laser.SetActive(true);
+        }
+        if(spinTimer >= spinTime)
+        {
+            isSpinning = false;
+            spinTimer = 0;
+            spinCooldownTimer = 0;
+            phase2Laser.SetActive(false);
+        }
+        yield return null;
     }
 }
