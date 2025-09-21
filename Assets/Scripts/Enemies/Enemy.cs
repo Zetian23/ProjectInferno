51fe6bdfb05b9e5a4ebb21422b97e16075b20857
wkstevens1993 <wkstevens1993@gmail.com>
2025-09-21 11:15:09 -0400

    Slime enemy fully created

--- Assets/Scripts/Enemies/Enemy.cs
+++ Assets/Scripts/Enemies/Enemy.cs
@@ -117,7 +117,7 @@ public class Enemy : MonoBehaviour, IDamage, IFreezable, ISavedData
     public virtual IEnumerator flashDamage()
     {
         model.material.color = Color.red;
-        yield return new WaitForSeconds(0.2f);
+        yield return new WaitForSeconds(0.1f);
         model.material.color = colorOrg;
     }
 
