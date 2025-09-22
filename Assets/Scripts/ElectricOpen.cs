bfcdb0b5c53e56f73b2a17f970e2324e34e3a023
Zetian23 <alexnewland12@gmail.com>
2025-09-22 18:37:42 -0400

    Add textures for door and chest

--- Assets/Scripts/ElectricOpen.cs
+++ Assets/Scripts/ElectricOpen.cs
@@ -6,10 +6,14 @@ public class Open : MonoBehaviour, IChargable
     [SerializeField] int maxCharge = 5;
     public Animator obj;
     [SerializeField]int currcharge;
+    [SerializeField] GameObject door;
+    [SerializeField] Material rend;
+
 
     void Start()
     {
         currcharge = 0;
+        Renderer renderer = door.GetComponent<Renderer>();
     }
     // Update is called once per frame
     void Update()
@@ -18,6 +22,7 @@ public class Open : MonoBehaviour, IChargable
         if(currcharge >= maxCharge)
         {
             obj.SetBool("Opening", true);
+            if(rend != null) door.GetComponent<Renderer>().material = rend;
         }
     }
 
