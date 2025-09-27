26f0e6fa4b4e71fc5f74e85ad98f66a06beb461f
Zetian23 <alexnewland12@gmail.com>
2025-09-27 14:01:05 -0400

    Made changes to house level

--- Assets/Scripts/EPresent.cs
+++ Assets/Scripts/EPresent.cs
@@ -2,13 +2,43 @@ using UnityEngine;
 
 public class EPresent : MonoBehaviour
 {
-    [SerializeField] GameObject detector;
-    public bool triggered = false;
-    public void OnTriggerStay(Collider other)
+    
+    public GameObject App;
+    public GameObject Dis;
+    GameObject[] enemies;
+    bool gone = false;
+
+    private void Update()
     {
-        if (other.CompareTag("Enemy")){
-            detector.SetActive(true);
-            triggered = true;
+        
+        if (App != null)
+        {
+            if (gone)
+            {
+                App.SetActive(true);
+                Destroy(this);
+            }
+            else
+            {
+                App.SetActive(false);
+            }
+        }
+
+        if (Dis != null)
+        {
+            if (gone)
+            {
+               Destroy(Dis);
+               Destroy(this);
+            }
+
         }
     }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        enemies = GameObject.FindGameObjectsWithTag("Enemy");
+       if(enemies.Length <= 0) { gone = true; }else { gone = false; }
+    }
+
 }
--- Assets/Scripts/StepSwitch.cs
+++ Assets/Scripts/StepSwitch.cs
@@ -3,18 +3,19 @@ using UnityEngine;
 public class StepSwitch : MonoBehaviour
 {
     public bool cho;
-    public Transform objPos;
+    public GameObject objPos;
     public GameObject gameObj;
-    public int tri = 0;
     private void OnTriggerStay(Collider other)
     {
-        if (other.CompareTag("Player") && tri == 0)
+        if (other.CompareTag("Player"))
         {
-            tri = 1;
+            
             if (cho == true)
             {
-                gameObj.transform.position = objPos.position;
-                gameObj.transform.rotation = objPos.rotation;
+                Instantiate(gameObj);
+                gameObj.transform.position = objPos.transform.position;
+                gameObj.transform.rotation = objPos.transform.rotation;
+                gameObj.transform.parent = objPos.transform;
             } else {
                 Destroy(gameObj);
             }
