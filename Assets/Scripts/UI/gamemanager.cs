e4923024b35611e750b4328a53a85e3a5bfd4ef7
Yormungarde <naokrin@gmail.com>
2025-09-28 17:23:16 -0400

    Fixing of tutorial timer
    The internal timer that displays the tutorial text was incorrect.

--- Assets/Scripts/UI/gamemanager.cs
+++ Assets/Scripts/UI/gamemanager.cs
@@ -74,6 +74,8 @@ public class gamemanager : MonoBehaviour
     float warningTimer;
     bool waveTextIsActive;
 
+    float tutorialTimer;
+
     public enum bossType { sloth, wrath, gluttony, envy, lust, greed };
     public bossType boss;
 
@@ -126,6 +128,16 @@ public class gamemanager : MonoBehaviour
         }
 
         displayAmmoConut();
+
+        if (tutorialTimer >= 0)
+        {
+            tutorialTimer -= Time.deltaTime;
+
+            if (tutorialTimer <= 0)
+            {
+                TutorialBox.SetActive(false);
+            }
+        }
     }
 
     public void statePause()
@@ -188,16 +200,10 @@ public class gamemanager : MonoBehaviour
     {
         tutorialText.text = message;
         TutorialBox.SetActive(true);
-        float timer = 10f;
-        while (timer > 0) {
-            timer -= Time.deltaTime;
-            if (timer <= 0)
-            {
-                TutorialBox.SetActive(false);
-            }
-        }
+        tutorialTimer = 7f;
 
     }
+
 
     public void displayAmmoConut()
     {
