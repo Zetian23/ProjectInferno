f6f03d64fe22895a70cdfc6202a60c6192bc67ad
wkstevens1993 <wkstevens1993@gmail.com>
2025-09-27 21:26:41 -0400

    minimap is fixed and follows the player

--- Assets/Scripts/UI/MiniMap.cs
+++ Assets/Scripts/UI/MiniMap.cs
@@ -8,7 +8,7 @@ public class MiniMap : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        player = GameObject.FindWithTag("Player").transform;
     }
 
     // Update is called once per frame
